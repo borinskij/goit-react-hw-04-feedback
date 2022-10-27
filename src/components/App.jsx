3fd922488ae20002f72fa50951541f1394cf3a17
Abdrii Borinskij <aborinskij@gmail.com>
2022-10-27 16:51:49 +0300

    json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,3 +29,36 @@ export class App extends Component {
     );
   }
 }
+//  import React from 'react'
+//  import PropTypes from 'prop-types'
+
+//  function App() {
+// const [] = useState(0)
+// state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0,
+// };
+// hendelClick = event => {
+//   const { name } = event.target;
+//   return this.setState({ [name]: this.state[name] + 1 });
+// };
+
+//    return (
+//      <div style={{ margin: 50 }}>
+//   <Section title="Please leave feedback">
+//     <FeedbackOptions
+//       hendelClick={this.hendelClick}
+//       options={Object.keys(this.state)}
+//     />
+//   </Section>
+//   <Section title="Statistics">
+//     <Statistics state={this.state} />
+//   </Section>
+// </div>
+//    )
+//  }
+
+//  App.propTypes = {}
+
+//  export default App
